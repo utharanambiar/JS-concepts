c8d2423fcf4d5de750ff8bd3b67501b2296e4987
utharanambiar <utharanambiar@gmail.com>
2025-01-01 19:57:50 +0530

    fix reduce polyfill

--- Polyfills/reducePolyfill.js
+++ Polyfills/reducePolyfill.js
@@ -1,15 +1,18 @@
 let arr = [1, 2, 3, 4];
 
 Array.prototype.myReduce = function (cb, initialVal) {
-  let acc = initialVal;
-  for (let i = 0; i < this.length; i++) {
-    if (acc !== undefined) {
-      acc = cb.call(this, acc, this[i], i, this);
-    } else {
-      acc = this[i];
-    }
+  let acc = arguments.length === 1 ? this[0] : initialVal;
+  let startIndex = arguments.length === 1 ? 1 : 0;
+  for (let i = startIndex; i < this.length; i++) {
+    acc = cb(acc, this[i], i, this);
   }
   return acc;
 };
 
-console.log(arr.myReduce((prev, curr) => prev + curr));
+console.log(arr.reduce((acc, curr) => acc + curr, 10));
+
+// console.log(
+//   [1, 2, 3, 4].reduce(function (acc, val) {
+//     return acc + val;
+//   }, undefined)
+// );
